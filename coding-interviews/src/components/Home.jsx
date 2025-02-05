b3fac0bdf6c4b93a0d95bb22187a3fd5c586e108
Asiama Phoebe <phoebeasiama6@gmail.com>
2025-02-05 23:58:26 +0000

    Add items

--- coding-interviews/src/components/Home.jsx
+++ coding-interviews/src/components/Home.jsx
@@ -1,8 +1,80 @@
-
+import { useState } from "react";
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState("");
+  const addToDo = () => {
+      console.log("I AM HERE")
+    if (text) {
+      setItems([...items, { text, completed: false }]);
+      console.log("items", items)
 
-export default Home
+      setText("");
+    }
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "start",
+        height: "100vh",
+        padding: "12px",
+        margin: "12px",
+      }}
+    >
+      <div>
+        <h1
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Welcome to my To-Do List
+        </h1>
+        <p>
+          Whe manage and schedule all your task in one place. Click the button
+          below to proceed
+        </p>
+      </div>
+      <div>
+        <input
+          type="text"
+          name="text"
+          placeholder="Enter your item here"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          style={{
+            padding: "12px",
+            margin: "12px",
+            width: "100%",
+            borderRadius: "10px",
+            border: "1px solid #ccc",
+          }}
+        />
+        <button
+          onClick={addToDo}
+          style={{
+            padding: "12px",
+            margin: "12px",
+            width: "50%",
+            borderRadius: "10px",
+            backgroundColor: "blue",
+            color: "white",
+            fontweight: "bold",
+            border: "none",
+            cursor: "pointer",
+          }}
+        >
+          + ADD
+        </button>
+      <ul>
+            {items.map((item, index) => {
+                  return <li key={index}>{item.text}</li>;
+            })}
+      </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
